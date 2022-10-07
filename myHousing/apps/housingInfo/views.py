f71027462cff70fd2b3adc4bd542a308d95f9de6
Edwin Ing <47252149+EdwinIngJ@users.noreply.github.com>
2022-10-07 15:53:35 -0500

    Fixed Housing Endpoint not Displaying

--- myHousing/apps/housingInfo/views.py
+++ myHousing/apps/housingInfo/views.py
@@ -3,10 +3,13 @@ from rest_framework import viewsets, serializers
 from .models import HousingInfo
 
 
-class HousingInfoSerializer(serializers.Serializer):
+class HousingInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = HousingInfo
         fields = ["housing_id", "housing_name", "housing_type", "image_filename"]
+
+    # def create(self, validated_data):
+    #     return HousingInfo.objects.create(**validated_data)
 
 
 # Create your views here.
