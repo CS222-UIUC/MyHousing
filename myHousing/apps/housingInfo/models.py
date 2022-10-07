dcaa67a247ea70bb4e321d16259b5df71cc32b2e
Edwin Ing <47252149+EdwinIngJ@users.noreply.github.com>
2022-10-07 13:57:30 -0500

    Added router to housingInfo

--- myHousing/apps/housingInfo/models.py
+++ myHousing/apps/housingInfo/models.py
@@ -2,18 +2,18 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from apps.reviews.models import Reviews
 
+
 class HousingInfo(models.Model):
     HousingId = models.BigAutoField(primary_key=True)
     HousingName = models.CharField(max_length=500)
-    
-    HOUSING_TYPE_CHOICES = [
-        
-    ]
-    HousingType = models.CharField(
-        max_length=2
-    )
+
+    HOUSING_TYPE_CHOICES = []
+    HousingType = models.CharField(max_length=2)
 
     ImageFileName = models.CharField(max_length=500)
+
+    def __str__(self) -> str:
+        return self.HousingName
 
     # HousingReviews will be an array of ReviewsId
     # HousingReviews = ArrayField(
--- myHousing/apps/housingInfo/views.py
+++ myHousing/apps/housingInfo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets, serializers
+from .models import HousingInfo
+
+
+class HousingInfoSerializer(serializers.Serializer):
+    class Meta:
+        model = HousingInfo
+        fields = ["HousingId", "HousingName", "HousingType", "ImageFileName"]
+
 
 # Create your views here.
+class HousingInfoViewSet(viewsets.ModelViewSet):
+    queryset = HousingInfo.objects.all()
+    serializer_class = HousingInfoSerializer
