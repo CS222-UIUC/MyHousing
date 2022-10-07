b5e002573250ccb789bbe59f88b5ad2926371283
justinklee345 <72401945+justinklee345@users.noreply.github.com>
2022-10-07 15:49:40 -0500

    Added DRF serializers to reviews model and started creating the api for reviews also started adding some of the endpoints of our REST procedures

--- myHousing/apps/housingInfo/models.py
+++ myHousing/apps/housingInfo/models.py
@@ -5,10 +5,10 @@ from apps.reviews.models import Reviews
 
 class HousingInfo(models.Model):
     housing_id = models.BigAutoField(primary_key=True)
-    housing_name = models.CharField(max_length=500)
+    housing_name = models.CharField(max_length=500, default="tem", editable=False)
 
     HOUSING_TYPE_CHOICES = []
-    housing_type = models.CharField(max_length=2)
+    housing_type = models.CharField(max_length=50)
 
     image_filename = models.CharField(max_length=500)
 
--- myHousing/apps/reviews/views.py
+++ myHousing/apps/reviews/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import ReviewsSerializer
+from .models import Reviews
+class ReviewsViewSet(viewsets.ModelViewSet):
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewsSerializer
 
 # Create your views here.
