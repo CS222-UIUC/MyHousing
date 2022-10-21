3a83c53020b295fc5c8e5208c867dd4e82dd527b
Edwin Ing <47252149+EdwinIngJ@users.noreply.github.com>
2022-10-21 18:34:51 -0500

    Added auth permissions to review/housinginfo views

--- myHousing/apps/housingInfo/views.py
+++ myHousing/apps/housingInfo/views.py
@@ -6,18 +6,19 @@ from .serializers import HousingInfoSerializer
 from .models import HousingInfo
 from apps.reviews.models import Reviews
 from apps.reviews.views import ReviewsSerializer
-from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.decorators import login_required
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from config.permissions import IsOwnerOrReadOnly
 
 
 class HousingInfoViewSet(viewsets.ModelViewSet):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = HousingInfo.objects.all()
     serializer_class = HousingInfoSerializer
 
 
 class ReviewsViewSet(viewsets.ViewSet):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
     def list(self, request, housinginfo_pk):
         queryset = Reviews.objects.filter(housing_info=housinginfo_pk)
         serializer = ReviewsSerializer(queryset, many=True)
--- myHousing/apps/reviews/views.py
+++ myHousing/apps/reviews/views.py
@@ -3,10 +3,14 @@ from rest_framework import viewsets
 
 from .serializers import ReviewsSerializer
 from .models import Reviews
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from config.permissions import IsOwnerOrReadOnly
 
 
 class ReviewsViewSet(viewsets.ModelViewSet):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
     queryset = Reviews.objects.all()
     serializer_class = ReviewsSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
