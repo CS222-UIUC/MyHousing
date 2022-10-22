8121467142f8d8f5c62e3eb0d87a7405db4955b5
justinklee345 <72401945+justinklee345@users.noreply.github.com>
2022-10-22 16:56:13 -0500

    Added tests for user and admin

--- myHousing/apps/users/admin.py
+++ myHousing/apps/users/admin.py
@@ -2,6 +2,16 @@
 # Django admin customization
 # """
 
+from django.contrib import admin
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "is_admin")
+
+
+admin.site.register(User, UserAdmin)
+
 # from django.contrib import admin
 # from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # from django.utils.translation import gettext_lazy as _
