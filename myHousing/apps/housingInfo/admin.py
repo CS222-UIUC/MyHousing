7a70b82162e6fd1448a834cadfa1fa5267b7f060
justinklee345 <72401945+justinklee345@users.noreply.github.com>
2022-11-01 16:37:09 -0500

    Added addresses inside

--- myHousing/apps/housingInfo/admin.py
+++ myHousing/apps/housingInfo/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import HousingInfo
+from .models import HousingInfo, Address
 
 # Register your models here.
 class HousingInfoAdmin(admin.ModelAdmin):
     list_display =  ("housing_id", "housing_name", "housing_types", "housing_price", "housing_location", "image_filename")
+    list_display_links = ["housing_location"]
 
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        "street_address",
+        "street_address_two",
+        "city",
+        "state",
+        "country",
+        "zip"
+    ]
 
 admin.site.register(HousingInfo, HousingInfoAdmin)
+admin.site.register(Address, AddressAdmin)
