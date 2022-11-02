edda1f7c1edbeb58081ab086957a18989cd670c5
Edwin Ing <47252149+EdwinIngJ@users.noreply.github.com>
2022-11-01 20:57:53 -0500

    Modified housinginfo model to upload data.

--- myHousing/apps/housingInfo/models.py
+++ myHousing/apps/housingInfo/models.py
@@ -4,15 +4,18 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+
 def upload_to(instance, filename):
     return "images/{filename}".format(filename=filename)
+
 
 def validate_zipcode(value):
     if value < 10000 or value > 99999:
         raise ValidationError(
-            _('%(value)s is not a valid zipcode!'),
+            _("%(value)s is not a valid zipcode!"),
             params={"value": value},
         )
+
 
 HOUSING_CHOICES = (
     ("1B", "1 Bedroom"),
@@ -22,21 +25,29 @@ HOUSING_CHOICES = (
     ("5B", "5 or More Bedroom"),
     ("1S", "Studio"),
 )
+
+
 class HousingInfo(models.Model):
     housing_id = models.BigAutoField(primary_key=True)
     housing_name = models.CharField(max_length=500, default="")
 
-    housing_types = ArrayField(models.CharField(max_length=2, choices=HOUSING_CHOICES, blank=True, null=True), blank=True, null=True) # Array of available housing_choices
+    housing_types = ArrayField(
+        models.CharField(max_length=2, choices=HOUSING_CHOICES, blank=True, null=True),
+        blank=True,
+        null=True,
+    )  # Array of available housing_choices
     housing_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     street_address = models.CharField(max_length=100, null=True, blank=True)
     street_address_two = models.CharField(max_length=100, null=True, blank=True)
     city = models.CharField(max_length=100, default="Champaign")
     state = models.CharField(max_length=100, default="Illinois")
-    #country will be default to US
+    # country will be default to US
     country = models.CharField(max_length=50, default="United States")
     zip = models.IntegerField(validators=[validate_zipcode], null=True, blank=True)
 
-    housing_description = models.CharField(max_length=500, default="This is where the description goes!")
+    housing_description = models.CharField(
+        max_length=5000, default="This is where the description goes!"
+    )
     # housing_reviews = ArrayField(models.ForeignKey)
 
     image_filename = models.ImageField(null=True, blank=True, upload_to=upload_to)
