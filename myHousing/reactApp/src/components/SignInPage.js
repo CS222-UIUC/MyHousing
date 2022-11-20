1f56363b4440c3c9c72eb27529a7a93999d941c0
Edwin Ing <47252149+EdwinIngJ@users.noreply.github.com>
2022-11-20 11:11:42 -0600

    Removed logs and ran prettier

--- myHousing/reactApp/src/components/SignInPage.js
+++ myHousing/reactApp/src/components/SignInPage.js
@@ -1,20 +1,15 @@
-import React, {Component} from "react";
-import {Link, Navigate} from "react-router-dom";
-import PropTypes from "prop-types"
-import {connect} from 'react-redux'
-import {
-    MDBContainer,
-    MDBInput,
-    MDBBtn,
-  }
-from 'mdb-react-ui-kit';
+import React, { Component } from "react";
+import { Link, Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { MDBContainer, MDBInput, MDBBtn } from "mdb-react-ui-kit";
 
-import {login} from "../actions/auth";
+import { login } from "../actions/auth";
 
 export class SignInPage extends Component {
   state = {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   };
 
   static propTypes = {
@@ -23,32 +18,51 @@ export class SignInPage extends Component {
   };
 
   onSubmit = (e) => {
-    console.log("BJFJSDLKF")
-    e.preventDefault(); 
+    console.log("BJFJSDLKF");
+    e.preventDefault();
     this.props.login(this.state.email, this.state.password);
   };
 
-  onChange = (e) => this.setState({[e.target.name]: e.target.value});
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
     if (this.props.isAuthenticated) {
-      return <Navigate to="/"/>;
+      return <Navigate to="/" />;
     }
-    const {email , password} = this.state;
+    const { email, password } = this.state;
 
     return (
-    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-    <h1>Sign-In Here</h1>
-    <form onSubmit={this.onSubmit}>
-      <MDBInput wrapperClass='mb-4' label='Email address' id='form1'  name="email" type='email'  onChange={this.onChange} value={email}/>
-      <MDBInput wrapperClass='mb-4' label='Password' id='form2' name="password" type='password' onChange={this.onChange} value={password}/>
+      <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+        <h1>Sign-In Here</h1>
+        <form onSubmit={this.onSubmit}>
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Email address"
+            id="form1"
+            name="email"
+            type="email"
+            onChange={this.onChange}
+            value={email}
+          />
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Password"
+            id="form2"
+            name="password"
+            type="password"
+            onChange={this.onChange}
+            value={password}
+          />
 
-
-      <MDBBtn type="submit" className="mb-4">Sign in</MDBBtn>
-    </form>
-    </MDBContainer>
-    )
-  
+          <MDBBtn type="submit" className="mb-4">
+            Sign in
+          </MDBBtn>
+          <p>
+            Don't have an account? <Link to="/registrationPage">Register</Link>
+          </p>
+        </form>
+      </MDBContainer>
+    );
   }
 }
 
@@ -56,30 +70,4 @@ const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
 });
 
-export default connect(mapStateToProps, {login})(SignInPage);
-// export default SignInPage;
-// import {
-//     MDBContainer,
-//     MDBInput,
-//     MDBCheckbox,
-//     MDBBtn,
-//     MDBIcon
-//   }
-//   from 'mdb-react-ui-kit';
-
-// function SignInPage() {
-//     return(
-
-//       <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-//           <h1>Sign-In Here</h1>
-//       <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
-//       <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
-
-
-//       <MDBBtn className="mb-4">Sign in</MDBBtn>
-
-//     </MDBContainer>
-//     );
-// }
-
-// export default SignInPage
+export default connect(mapStateToProps, { login })(SignInPage);
